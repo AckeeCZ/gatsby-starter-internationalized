cd0f520fff2f985887dd4192944394f2970bda6d
Marek Janča <marekjanca@Marek-MacBook-Air.local>
2019-06-02 19:59:50 +0200

    throw an error on no default lang

--- src/i18n/utils/getLocalizedPath.js
+++ src/i18n/utils/getLocalizedPath.js
@@ -2,6 +2,12 @@ const languages = require("../config/languages");
 
 const defaultLanguage = languages.find(lang => lang.default);
 
+if (!defaultLanguage) {
+    throw new Error(
+        "Default language has to be specified in the language configuration."
+    );
+}
+
 const getLocalizedPath = (originalPath, locale) => {
     const keyPath = originalPath.replace(/(\w+)\/$/, "$1");
     const lang = languages.find(lang => lang.locale === locale);
