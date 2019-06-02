058867118aab84dba194cb1c136f573cf81cb005
Marek Janča <marekjanca@Marek-MacBook-Air.local>
2019-06-02 20:09:19 +0200

    throw an error on no route for an origin path

--- src/i18n/utils/getLocalizedPath.js
+++ src/i18n/utils/getLocalizedPath.js
@@ -11,11 +11,16 @@ if (!defaultLanguage) {
 const getLocalizedPath = (originalPath, locale) => {
     const keyPath = originalPath.replace(/(\w+)\/$/, "$1");
     const lang = languages.find(lang => lang.locale === locale);
-    const localizedPath =
-        locale === defaultLanguage.locale
-            ? defaultLanguage.routes[keyPath]
-            : `/${locale}${lang.routes[keyPath]}`;
-    return localizedPath;
+
+    const isDefault = locale === defaultLanguage.locale;
+    const localizedPath = lang && lang.routes && lang.routes[keyPath];
+
+    if (!localizedPath) {
+        throw new Error(
+            `You have to specify a tranlastion for all of your routes.\n There is no transalation for ${originalPath} in language ${locale}.`
+        );
+    }
+    return isDefault ? localizedPath : `/${locale}${localizedPath}`;
 };
 
 module.exports = getLocalizedPath;
